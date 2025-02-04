8c3829a39a9ca66e3957bcb2963833e13e8ef048
jrMTZ001 <a22390070@ucslp.net>
2025-02-04 15:48:07 -0600

    Menú Principal y más

---
+++ Iron Spear Protocol/Assets/C#/UI/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Salir");
+        Application.Quit();
+    }
+}
